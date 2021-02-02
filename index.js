352009af6c249e80cbe9552f425f08653ce2c5d9
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-02-02 23:37:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ try {
 }
 const {MongoClient} = require('mongodb');
 const uri ="mongodb+srv://kingbaruk:H2MWiHQgN46qrUu>@cluster0.9vx1c.mongodb.net/test?retryWrites=true&w=majority";
-
+dotenv.config()
 
 async function listDatabases(client){
 
@@ -103,6 +103,7 @@ async function findOneListingByName(client, nameOfListing) {
 async function dbconnect(){
 	const uri = 'mongodb+srv://kingbaruk:H2MWiHQgN46qrUu@cluster0.9vx1c.mongodb.net/test?retryWrites=true&w=majority';
 	console.log(uri);
+	console.log("test");
 	global.dbclient = new MongoClient(uri, { useNewUrlParser: true });
 	
 	try {
