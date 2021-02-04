cd7e2b9b65c92c9e189ffbef8bffd16114100c56
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-02-04 13:27:14 +0100

    Update chatplugins-quotes.js

--- commands/chatplugins-quotes.js
+++ commands/chatplugins-quotes.js
@@ -273,6 +273,9 @@ async function infofunc (arg, by, room, cmd,vart) {
 			if(bitbals["nederlands"]==undefined){
 				bitbals["nederlands"]={};
 			}
+			if(bitbals["nederlands"][args[0]]==undefined){
+				bitbals["nederlands"][args[0]]=0;
+			}
 			bitbals["nederlands"].args[0]=bitbals["nederlands"].args[0]+args[1];
 			vart.reply(args[0]+"heeft " +bitbals["nederlands"].args[0]+" bitterballen");
 			
