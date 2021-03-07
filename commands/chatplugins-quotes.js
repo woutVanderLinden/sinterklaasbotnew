265fe82d3f97c474b695333959ae55965b0e5dbe
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-03-07 21:01:49 +0100

    Update chatplugins-quotes.js

--- commands/chatplugins-quotes.js
+++ commands/chatplugins-quotes.js
@@ -196,8 +196,11 @@ async function infofunc (arg, by, room, cmd,vart) {
 			}
 			if(infos["nederlands"][args[0]]==undefined){
 				infos["nederlands"][args[0]]="";
+					infos["nederlands"][args[0]]=infos["nederlands"][args[0]]+" "+args[1];
+			}else{
+					infos["nederlands"][args[0]]=infos["nederlands"][args[0]]+", "+args[1];
 			}
-			infos["nederlands"][args[0]]=infos["nederlands"][args[0]]+" "+args[1];
+			//infos["nederlands"][args[0]]=infos["nederlands"][args[0]]+" "+args[1];
 			vart.reply("added info to "+ args[0]);
 			
 			
