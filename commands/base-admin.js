34a5410cd1df3cece2ceba85c4e854ff26ed3558
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-06-07 21:52:38 +0200

    Update base-admin.js

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -692,7 +692,7 @@ exports.commands = {
 					draftmons["tierlist"]["Tier"+i]["pokemon"]=removeItemOnce(draftmons["tierlist"]["Tier"+i]["pokemon"],arg);
 					var pointscost=draftmons["tierlist"]["Tier"+i]["points"];
 					var currentscore=global.users[name]["erekredieten"];
-					var picksleft=draftmons["freepicks"]-global.picknr[toId(room)];
+					var picksleft=draftmons["freepicks"]-global.picknr[toId(room)]-1;
 					console.log("freepicks "+draftmons["freepicks"]+" picknr: "+global.picknr[toId(room)]+" pickleft"+picksleft);
 					if(picksleft*40>=currentscore-pointscost){
 						return this.reply("please make sure you have at least "+picksleft*40+ "Erekredieten left" );
