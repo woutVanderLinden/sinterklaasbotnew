f80594d368b88197046cc2942915d42047bb8094
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-06-09 11:31:41 +0200

    Update bot-setup.js

--- bot-setup.js
+++ bot-setup.js
@@ -24,10 +24,11 @@ var sys = require('sys');
 var url = require('url');
 var http = require('http');
 try {
-var XLSX = require('xlsx');
+		var XLSX=require('child_process').spawnSync('sh', ['-c', 'npm install xlsx'], {stdio: 'inherit'});
+	var XLSX = require('xlsx');
 } catch (e) {
 	console.log('Installing dependencies... (xlsx)');
-	require('child_process').spawnSync('sh', ['-c', 'npm install xlsx'], {stdio: 'inherit'});
+	var XLSX=require('child_process').spawnSync('sh', ['-c', 'npm install xlsx'], {stdio: 'inherit'});
 }
 function scan (p) {
 	return readline.question(p);
