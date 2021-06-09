dbc315e353bcfef78614443c0d22a761ec12dbf6
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-06-09 10:01:23 +0200

    Update base-admin.js

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -1090,20 +1090,20 @@ exports.commands = {
 		var reader = new FileReader();
 
     		reader.onload = function(e) {
-      		var data = e.target.result;
-     		 var workbook = XLSX.read(data, {
-       			 type: 'binary'
-      		});
-		var sheetName="test.xlsx";
-     	 	workbook.SheetNames.forEach(function(sheetName) {
-        	// Here is your object
-        	var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-        	var json_object = JSON.stringify(XL_row_object);
-        	console.log(json_object);
+			var data = e.target.result;
+			 var workbook = XLSX.read(data, {
+				 type: 'binary'
+			});
+			var sheetName="test.xlsx";
+			workbook.SheetNames.forEach(function(sheetName) {
+			// Here is your object
+			var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+			var json_object = JSON.stringify(XL_row_object);
+			console.log(json_object);
 
-     	 	})
-
-    	},
+			})
+		}
+	},
 
   
   
