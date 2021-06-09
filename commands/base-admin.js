b7a3b8f900294cefefc8b910d0dd5aaf05f49bcb
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-06-09 10:48:37 +0200

    Update base-admin.js

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -1094,10 +1094,11 @@ exports.commands = {
 			});
 			var sheetName="test.xlsx";
 			workbook.SheetNames.forEach(function(sheetName) {
-			// Here is your object
-			var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-			var json_object = JSON.stringify(XL_row_object);
-			console.log(json_object);
+				// Here is your object
+				var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+				var json_object = JSON.stringify(XL_row_object);
+				console.log(json_object);
+			}
 
 		
 	},
