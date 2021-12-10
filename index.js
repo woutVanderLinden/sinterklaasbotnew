51265b72b84b758e384782d78cb23131cc5e785c
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-12-10 11:32:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -387,7 +387,7 @@ var opts = {
 function startKeepAlive() {
     setInterval(function() {
         var options = {
-            host: 'newsinterklaasbot.herokuapp.com',
+            host: 'sinterklaasbot.herokuapp.com',
             port: 80,
             path: '/'
         };
