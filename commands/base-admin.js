bccc57ac14434f64e5ab4dcef5fc640e359d6128
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2021-12-10 13:43:05 +0100

    Update base-admin.js

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -1826,8 +1826,7 @@ function generateMonsList(monlist){
 	var stopped=false;
 	var i=1;
 	while(!stopped) {
-
-		resultlist.push(pickmultimons(monlist["tierlist"]["Tier"+i]["picks"]),monlist["tierlist"]["Tier"+i]["pokemon"]),monlist);	
+		resultlist.push(pickmultimons(monlist["tierlist"]["Tier"+i]["picks"],monlist["tierlist"]["Tier"+i]["pokemon"],monlist)));	
 		i++;
 		if(i>monlist["length"]){
 			stopped=true;
