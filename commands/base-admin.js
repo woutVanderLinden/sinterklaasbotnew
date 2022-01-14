b1192b067e91857747a66d2bd3df1c090084508e
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2022-01-14 21:06:49 +0100

    updated giftdraft

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -60,8 +60,9 @@ function startNewGiftTier(replier,room) {
 	global.draftdirectionup[toId(room)]= !global.draftdirectionup[toId(room)];
 	global.nrdrafted=0;
 	global.monslists=[];
-	global.picknr[toId(room)]=0;
+	global.users={};
 	if(0 == global.currenttier[toId(room)]){
+		global.monslists=[];
 		return replier.reply("the draft is over")
 	}
 	global.tierPicks=global.todraftmons[toId(room)]["tierlist"]["Tier"+global.currenttier[toId(room)]]["picks"];
