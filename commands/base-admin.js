f33551824770dde9a834150b4d113beab93e20b3
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2022-01-15 11:52:53 +0100

    updated giftdraft

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -1364,7 +1364,8 @@ exports.commands = {
 	
 	},
 	creategiftdraft:function (arg, by, room, cmd) {
-		global.draftroom= toId(room);
+		global.draftroom= room;
+		console.log(global.draftroom);
 		this.reply("!htmlbox <p> hi </p>");
 		this.send(global.draftroom, '!htmlbox  <h1>Giftdraft</h1> <p>Press this button or ?joindraft to join </p> <button name="send" value="/msgroom nederlands, /botmsg sinterklaas, ?joindraft"> joindraft </button>' );
 
