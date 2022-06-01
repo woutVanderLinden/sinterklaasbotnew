e7832025acfb8d7a4f70d4a2de3bb55cfbd0063c
kingbaruk <waltlindder@gmail.com>
2022-06-01 10:25:35 +0200

    editing draftable

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -611,7 +611,7 @@ exports.commands = {
 			pmlists(global.monslists,room, this);
 			return;
 		}
-		var list=global.turnorder[toId(room)];
+		var list=global.turnorder;
 		var val= global.tierPicks-global.picknr[toId(room)];
 		var toreply= "!htmlbox Tier"+ arg + "drafter "+ list[global.nextdrafter] + " picksleft:" +val;
 		if(arg==""){
