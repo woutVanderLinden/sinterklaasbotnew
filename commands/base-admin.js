1bce7f3425d1f30774da9a2252e0a06cab085c8e
kingbaruk <waltlindder@gmail.com>
2022-06-01 11:22:22 +0200

    editing draftable

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -1221,7 +1221,7 @@ exports.commands = {
 			}
 		} 
 		else{
-			var draftmons=global.todraftmons[toId(room)];
+			var draftmons=global.todraftmons[globals.draftroom];
 			var i=1;
 			while(i<=draftmons["length"]){
 				var possiblepic=draftmons["tierlist"]["Tier"+i]["pokemon"];
