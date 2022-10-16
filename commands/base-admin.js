f7df7eeba259eb78f9f3af03bc4335f4d4220c49
kingbaruk <waltlindder@gmail.com>
2022-10-16 13:07:14 +0200

    editing draftable

--- commands/base-admin.js
+++ commands/base-admin.js
@@ -1467,7 +1467,7 @@ exports.commands = {
 		arg=arg.substring(1,arg.length);
 		if(global.auctionDrafting){
 			var index= global.draftvalues.turnorder.indexOf(name);
-			
+
 			global.passedusers.length = 0;
 			var draftmons=global.draftvalues.todraftmons[toId(global.draftvalues.draftroom)];
 			var i=1;
