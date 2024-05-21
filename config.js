e815a64ea1597f3fd71637e27bd16288959a590a
woutVanderLinden <32461171+woutVanderLinden@users.noreply.github.com>
2024-05-21 19:10:46 +0200

    .

--- config.js
+++ config.js
@@ -11,11 +11,12 @@
 *
 */
 
-exports.server = 'sim3.psim.us';
+//exports.server = 'sim3.psim.us';
+exports.server = 'localhost';
+exports.port = 8000;
 
-exports.port = 81;
-
-exports.serverid = 'showdown';
+exports.serverid = 'localhost';
+//exports.serverid = 'showdown';
 
 exports.autoReconnectDelay = 10 * 1000;
 exports.connectionTimeout = 2 * 60 * 1000;
